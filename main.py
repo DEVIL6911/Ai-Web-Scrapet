2a5b5af7ce2a5e34ed004c22d612cb32ecd60f71
Ayush Gupta <guptaop34495@gmail.com>
2025-11-05 15:40:40 +0530

    Update title and logging messages in main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ from scrape import (
 )
 from parse import parse_with_gemini
 
-st.title("🕸️ AI Web Scraper ")
+st.title("AI Web Scraper Macro project Ayush Gupta And Anika Divi ")
 
 url = st.text_input("Enter Website URL")
 
 # Step 1: Scrape the Website
 if st.button("Scrape Website"):
     if url:
-        st.write("🔄 Scraping the website...")
+        st.write(" Scraping the website...")
         try:
             dom_content = scrape_website(url)
             body_content = extract_body_content(dom_content)
@@ -29,15 +29,16 @@ if st.button("Scrape Website"):
 
 # Step 2: Parse Content with Gemini
 if "dom_content" in st.session_state:
-    parse_description = st.text_area("🧠 Describe what you want to extract")
+    parse_description = st.text_area(" Describe what you want to extract")
 
     if st.button("Parse Content"):
         if parse_description:
-            st.write("✨ Parsing content using Gemini 2.5 Flash...")
+            st.write(" Parsing content ollama...")
             try:
                 dom_chunks = split_dom_content(st.session_state.dom_content)
                 parsed_result = parse_with_gemini(dom_chunks, parse_description)
-                st.success("✅ Parsing complete!")
+                st.success("Parsing complete!")
                 st.text_area("Parsed Result", parsed_result, height=300)
             except Exception as e:
                 st.error(f"Error parsing with Gemini: {e}")
+
